8fe7cc1bd5cda2375651f378baf3df11e6fcc0b7
Nhat Quang <tommyquanglowkey2011@gmail.com>
2024-02-21 09:45:45 +0700

    Add playground

--- app.py
+++ app.py
@@ -2,3 +2,17 @@
 # Maybe flask, idk, hu ac hu ce
 # Where should i host? heroku?
 # Maybe torchserve is cool
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    load_dotenv()  # take environment variables from .env.
+    app.run(debug=True)
+
