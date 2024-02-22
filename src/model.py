cffceb209a4553ae81d2e5d22ab8d355a8f56f95
Nhat Quang <tommyquanglowkey2011@gmail.com>
2024-02-22 11:00:12 +0700

    WTF

--- src/model.py
+++ src/model.py
@@ -122,3 +122,31 @@ def create_mask(src, tgt):
     tgt_padding_mask = (tgt == config["PAD_IDX"])
     return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
     
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    src = src.to(DEVICE)
+    src_mask = src_mask.to(DEVICE)
+    
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(DEVICE)
+    for i in range(max_len-1):
+        memory = memory.to(DEVICE)
+        tgt_mask = (generate_square_subsequent_mask(ys.size(1))
+                    .type(torch.bool)).to(DEVICE)
+        out = model.decode(ys, memory, tgt_mask)
+        out = out.transpose(0, 1)
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim = 1)
+        next_word = next_word[-1].item()
+        
+        ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+        if next_word==config["EOS_IDX"]:
+            break
+    return ys
+
+def translate(model, src_sentence):
+    model.eval()
+    src = text_transform[SRC_LANGUAGE](src_sentence).view(1, -1)
+    num_tokens = src.shape[1]
+    src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
+    tgt_tokens = greedy_decode(model,  src, src_mask, max_len=num_tokens + 5, start_symbol=config["BOS_IDX"]).flatten()
+    return " ".join(vocab_transform[TGT_LANGUAGE].lookup_tokens(list(tgt_tokens.cpu().numpy()))).replace("<bos>", "").replace("<eos>", "")
