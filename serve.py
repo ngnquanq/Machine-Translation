384373b3eba90e218076ea1ab56224857b270f95
Nhat Quang <tommyquanglowkey2011@gmail.com>
2024-02-29 09:21:57 +0700

    stupid af - improve later

--- serve.py
+++ serve.py
@@ -7,19 +7,17 @@ import io
 import os
 import utils
 from flask import Flask, request, jsonify, render_template
+from sandbox import _load_model, translate,greedy_decode
+import time
 
-
-# Initial model
-global transformer
-transformer = None
 
 # Initialize our Flask application and the PyTorch model.
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 # Initial route for 1 API
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route("/predict", methods=["POST"])
 def _predict():
@@ -27,19 +25,31 @@ def _predict():
     data = {"success": False}
     # Ensure an text was properly uploaded to our endpoint
     if flask.request.method == "POST":
-        if flask.request.files.get("text"):
+        if flask.request.json.get("text"):
             # Read the text in the request
-            text = flask.request.files["text"].read()
-            text = text.decode("utf-8")
+            text = flask.request.json["text"]
             # Make the prediction
-            data["response"] = ...
+            translated_text = translate(transformer, text, source_text, target_vocab)
+            data["response"] = translated_text
             # Indicate that the request was a success
             data["success"] = True
-    return json.dumps(data, ensure_ascii=False)
+    return jsonify({"response": data["response"],
+                    "success": data["success"]})
 
 if __name__=="__main__":
     print("App is running")
-    #Load model
-    transformer, source_vocab, target_vocab, source_text, target_text = utils._load_model()
-    print("Model loaded")
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Device: ", DEVICE)
+    # Load the models
+    start_time = time.time()
+    try:
+        transformer, source_vocab, target_vocab, source_text, target_text = _load_model()
+        print("Model loaded")
+        transformer.to(DEVICE)
+    except Exception as e:
+        print("Error loading model: ", e)    
+    end_time = time.time()
+    print(f"Model loaded in {end_time - start_time} seconds")
     app.run(debug=True)
+    
+    
